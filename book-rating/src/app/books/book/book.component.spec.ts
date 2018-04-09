5e2fb36077ab80d5b189a426df94a6272450c6f6
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-04-09 20:23:17 +0200

    8

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -2,6 +2,7 @@ import { Book } from './../../shared/book';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { spy, verify } from 'ts-mockito';
 import { BookComponent } from './book.component';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 describe('BookComponent', () => {
   let component: BookComponent;
@@ -9,80 +10,119 @@ describe('BookComponent', () => {
   let element: HTMLElement;
   let book: Book;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        declarations: [BookComponent]
-      }).compileComponents();
-    })
-  );
+  describe('normal configuration', () => {
+    beforeEach(
+      async(() => {
+        TestBed.configureTestingModule({
+          declarations: [BookComponent]
+        }).compileComponents();
+      })
+    );
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BookComponent);
-    component = fixture.componentInstance;
-    element = fixture.nativeElement;
-    book = {
-      title: 'booktitle',
-      description: 'bookdesc',
-      rating: 3
-    } as Book;
-    component.book = book;
+    beforeEach(() => {
+      fixture = TestBed.createComponent(BookComponent);
+      component = fixture.componentInstance;
+      element = fixture.nativeElement;
+      book = {
+        title: 'booktitle',
+        description: 'bookdesc',
+        rating: 3
+      } as Book;
+      component.book = book;
+    });
+
+    it('should create', () => {
+      expect(() => fixture.detectChanges()).not.toThrow();
+      expect(component).toBeTruthy();
+    });
+
+    it('should display the description', () => {
+      fixture.detectChanges();
+      const descElem = element.querySelector('p.description');
+      expect(descElem).toBeTruthy();
+      expect(descElem.textContent).toEqual(book.description);
+    });
+
+    it('should display the title', () => {
+      fixture.detectChanges();
+      const titleElem = element.querySelector('span.title');
+      expect(titleElem).toBeTruthy();
+      expect(titleElem.textContent).toEqual(book.title);
+    });
+
+    it('should display the rating cats', () => {
+      component.ngOnChanges();
+      fixture.detectChanges();
+      const ratingCats = element.querySelectorAll('img.rating');
+      expect(ratingCats).toBeTruthy();
+      expect(ratingCats.length).toEqual(3);
+    });
+
+    it('should display rating buttons', () => {
+      fixture.detectChanges();
+      expect(element.querySelector('button.testing-rate-up')).toBeTruthy();
+      expect(element.querySelector('button.testing-rate-down')).toBeTruthy();
+    });
+
+    it('should call rateUp when rate-up button is clicked', () => {
+      fixture.detectChanges();
+      const compSpy = spy(component);
+
+      verify(compSpy.rateUp()).never();
+
+      const rateUpButton = element.querySelector('button.testing-rate-up') as HTMLButtonElement;
+      rateUpButton.click();
+
+      verify(compSpy.rateUp()).once();
+    });
+
+    it('should call rateDown when rate-down button is clicked', () => {
+      fixture.detectChanges();
+      const compSpy = spy(component);
+
+      verify(compSpy.rateDown()).never();
+
+      const rateDownButton = element.querySelector('button.testing-rate-down') as HTMLButtonElement;
+      rateDownButton.click();
+
+      verify(compSpy.rateDown()).once();
+    });
   });
 
-  it('should create', () => {
-    expect(() => fixture.detectChanges()).not.toThrow();
-    expect(component).toBeTruthy();
-  });
+  describe('default change detection', () => {
+    beforeEach(
+      async(() => {
+        TestBed.configureTestingModule({
+          declarations: [BookComponent]
+        })
+          .overrideComponent(BookComponent, {
+            set: { changeDetection: ChangeDetectionStrategy.Default }
+          })
+          .compileComponents();
+      })
+    );
 
-  it('should display the description', () => {
-    fixture.detectChanges();
-    const descElem = element.querySelector('p.description');
-    expect(descElem).toBeTruthy();
-    expect(descElem.textContent).toEqual(book.description);
-  });
+    beforeEach(() => {
+      fixture = TestBed.createComponent(BookComponent);
+      component = fixture.componentInstance;
+      element = fixture.nativeElement;
+      book = {
+        title: 'booktitle',
+        description: 'bookdesc',
+        rating: 3
+      } as Book;
+      component.book = book;
+    });
 
-  it('should display the title', () => {
-    fixture.detectChanges();
-    const titleElem = element.querySelector('span.title');
-    expect(titleElem).toBeTruthy();
-    expect(titleElem.textContent).toEqual(book.title);
-  });
+    it('should update the rating cats accordingly', () => {
+      component.ngOnChanges();
+      fixture.detectChanges();
+      expect(element.querySelectorAll('img.rating').length).toEqual(3);
 
-  it('should display the rating cats', () => {
-    component.ngOnChanges();
-    fixture.detectChanges();
-    const ratingCats = element.querySelectorAll('img.rating');
-    expect(ratingCats).toBeTruthy();
-    expect(ratingCats.length).toEqual(3);
-  });
-
-  it('should display rating buttons', () => {
-    fixture.detectChanges();
-    expect(element.querySelector('button.testing-rate-up')).toBeTruthy();
-    expect(element.querySelector('button.testing-rate-down')).toBeTruthy();
-  });
-
-  it('should call rateUp when rate-up button is clicked', () => {
-    fixture.detectChanges();
-    const compSpy = spy(component);
-
-    verify(compSpy.rateUp()).never();
-
-    const rateUpButton = element.querySelector('button.testing-rate-up') as HTMLButtonElement;
-    rateUpButton.click();
-
-    verify(compSpy.rateUp()).once();
-  });
-
-  it('should call rateDown when rate-down button is clicked', () => {
-    fixture.detectChanges();
-    const compSpy = spy(component);
-
-    verify(compSpy.rateDown()).never();
-
-    const rateDownButton = element.querySelector('button.testing-rate-down') as HTMLButtonElement;
-    rateDownButton.click();
-
-    verify(compSpy.rateDown()).once();
+      component.book = { ...book, rating: 5 };
+      component.ngOnChanges();
+      fixture.detectChanges();
+      expect(element.querySelectorAll('img.rating').length).toEqual(5);
+    });
   });
 });
--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -6,21 +6,21 @@ import { Component, Input, NO_ERRORS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'br-book',
-  template: `{{ b | json }}`
+  template: `{{ book | json }}`
 })
 class DummyBookComponent {
-  @Input() b: Book;
+  @Input() book: Book;
 }
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let element: HTMLElement;
 
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
         declarations: [DashboardComponent, DummyBookComponent]
-        // schemas: [NO_ERRORS_SCHEMA]
       }).compileComponents();
     })
   );
@@ -28,11 +28,19 @@ describe('DashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-    component.books = [{}, {}] as Book[];
+    element = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should display books', () => {
+    fixture.detectChanges();
+    // component.books = [{}, {}] as Book[];
+    const bookElements = element.querySelectorAll('br-book');
+    expect(bookElements).toBeTruthy();
+    expect(bookElements.length).toEqual(4);
   });
 });
